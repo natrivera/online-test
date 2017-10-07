edb03170abfdccf6dc68fe79b9d2c2993b85e19a
Natividad Rivera <riveranatividad1@gmail.com>
2017-10-07 14:28:39 -0700

    Add files via upload

---
+++ js/categories.js
@@ -0,0 +1,10 @@
+var cats = {
+  test_categories: [
+    { id: 9, name: "General Math", url: "" },
+    { id: 10, name: "Statistics", url: "" },
+    { id: 11, name: "Finance, Real Estate, And Law", url: "" },
+    { id: 12, name: "Computer and Information Systems", url: "" },
+    { id: 13, name: "technology and Operations Management", url: "" },
+    { id: 14, name: "Accounting", url: "" }
+  ]
+};
---
+++ js/math.js
@@ -0,0 +1,165 @@
+var jsonobject = {
+  response_code: 0,
+  results: [
+    {
+      category: "Basic Math",
+      type: "multiple",
+      difficulty: "easy",
+      question: "10 + 10 =",
+      correct_answer: "20",
+      incorrect_answers: ["11", "100", "30"]
+    },
+    {
+      category: "Basic Math",
+      type: "multiple",
+      difficulty: "easy",
+      question: "2 + 2 = ",
+      correct_answer: "4",
+      incorrect_answers: ["0", "10", "8"]
+    },
+    {
+      category: "Basic Math",
+      type: "boolean",
+      difficulty: "easy",
+      question: "41 - 21 = 20",
+      correct_answer: "True",
+      incorrect_answers: ["False"]
+    },
+    {
+      category: "Basic Math",
+      type: "multiple",
+      difficulty: "easy",
+      question: "1000 / 10",
+      correct_answer: "100",
+      incorrect_answers: ["1", "10", "1000"]
+    },
+    {
+      category: "Basic Math",
+      type: "multiple",
+      difficulty: "easy",
+      question: "25^(1/2)",
+      correct_answer: "5",
+      incorrect_answers: ["4", "3", "6"]
+    },
+    {
+      category: "Basic Math",
+      type: "multiple",
+      difficulty: "easy",
+      question: "25 + 25 = ",
+      correct_answer: "50",
+      incorrect_answers: ["0", "5", "10"]
+    },
+    {
+      category: "Basic Math",
+      type: "multiple",
+      difficulty: "easy",
+      question: "72 / 8 = ",
+      correct_answer: "9",
+      incorrect_answers: ["7", "5", "10"]
+    },
+    {
+      category: "Basic Math",
+      type: "multiple",
+      difficulty: "easy",
+      question: "11 x 11 = ",
+      correct_answer: "121",
+      incorrect_answers: ["111", "144", "100"]
+    },
+    {
+      category: "Basic Math",
+      type: "multiple",
+      difficulty: "easy",
+      question: "8 + 0 =",
+      correct_answer: "8",
+      incorrect_answers: ["10", "9", "0"]
+    },
+    {
+      category: "Basic Math",
+      type: "multiple",
+      difficulty: "easy",
+      question: "6 - 5 = ",
+      correct_answer: "1",
+      incorrect_answers: ["0", "12", "11"]
+    },
+    {
+      category: "Basic Math",
+      type: "multiple",
+      difficulty: "easy",
+      question: "10 + 10 =",
+      correct_answer: "20",
+      incorrect_answers: ["11", "100", "30"]
+    },
+    {
+      category: "Basic Math",
+      type: "multiple",
+      difficulty: "easy",
+      question: "2 + 2 = ",
+      correct_answer: "4",
+      incorrect_answers: ["0", "10", "8"]
+    },
+    {
+      category: "Basic Math",
+      type: "boolean",
+      difficulty: "easy",
+      question: "41 - 21 = 20",
+      correct_answer: "True",
+      incorrect_answers: ["False"]
+    },
+    {
+      category: "Basic Math",
+      type: "multiple",
+      difficulty: "easy",
+      question: "1000 / 10",
+      correct_answer: "100",
+      incorrect_answers: ["1", "10", "1000"]
+    },
+    {
+      category: "Basic Math",
+      type: "multiple",
+      difficulty: "easy",
+      question: "25^(1/2)",
+      correct_answer: "5",
+      incorrect_answers: ["4", "3", "6"]
+    },
+    {
+      category: "Basic Math",
+      type: "multiple",
+      difficulty: "easy",
+      question: "25 + 25 = ",
+      correct_answer: "50",
+      incorrect_answers: ["0", "5", "10"]
+    },
+    {
+      category: "Basic Math",
+      type: "multiple",
+      difficulty: "easy",
+      question: "72 / 8 = ",
+      correct_answer: "9",
+      incorrect_answers: ["7", "5", "10"]
+    },
+    {
+      category: "Basic Math",
+      type: "multiple",
+      difficulty: "easy",
+      question: "11 x 11 = ",
+      correct_answer: "121",
+      incorrect_answers: ["111", "144", "100"]
+    },
+    {
+      category: "Basic Math",
+      type: "multiple",
+      difficulty: "easy",
+      question: "8 + 0 =",
+      correct_answer: "8",
+      incorrect_answers: ["10", "9", "0"]
+    },
+    {
+      category: "Basic Math",
+      type: "multiple",
+      difficulty: "easy",
+      question: "6 - 5 = ",
+      correct_answer: "1",
+      incorrect_answers: ["0", "12", "11"]
+    }
+  ]
+};
